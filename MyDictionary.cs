c81f72b3d1a7009d44e03b8fe1b805489701c64c
orhan <orhan@DESKTOP-83EL6NB>
2021-01-23 19:46:24 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev23012021
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+
+        }
+        public void Add(TKey key, TValue value)
+        {
+           
+
+            TKey[] TempKeys = keys;
+            TValue[] TempValue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < TempKeys.Length; i++)
+            {
+                keys[i] = TempKeys[i];
+                values[i] = TempValue[i];
+
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Odev23012021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+            ogrenci.Add(1, "Orhan");
+            ogrenci.Add(2, "Muhammet");
+            Console.WriteLine(ogrenci.Length);
+                        
+        }
+    }
+}
